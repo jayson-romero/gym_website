dcab42c8b9d44f643e99c5684a155631e48b160e
ItonRomero <jaysonromero1218@gmail.com>
2023-04-10 22:02:49 +0800

    added exercises section

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+
 import { Link } from 'react-router-dom'
 
-const ExerciseCard = () => {
+const ExerciseCard = ({exercise}) => {
   return (
-    <div>ExerciseCard</div>
+    <Link to={`/exersice/${exercise.id}`} className="exercise-card md:mr-[20px]">
+        <img src={exercise.gifUrl} alt={exercise.name} Loading="lazy"/>
+        <div className='flex gap-[10px]'>
+            <buton className="text-[#fff] bg-red text-[14px] rounded-lg capitalize px-[21px] py-[5px]">{exercise.bodyPart}</buton>
+
+            <buton className="text-[#fff] bg-red text-[14px] rounded-lg capitalize px-[21px] py-[5px]">{exercise.target}</buton>
+        </div>
+        <p className='bold text-[21px] mt-[11px] mb-[10px] capitalize'>{exercise.name}</p>
+    </Link>
   )
 }
 
--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,12 +1,27 @@
 import React, {useEffect, useState} from 'react';
-
+import { Pagination } from '@mui/material'
 import { exercisesOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
+import styles from '../style';
 
 const Exercises = ({setExercises, bodyPart, exercises}) => {
 
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const exercisesPerPage = 9
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value ) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth' })
+  }
+
   return (
-    <div id='exercises' className='mt-[50px]'>
+    <div id='exercises' className={` ${styles.boxWidth} mt-[50px] `}>
 
       <h1 className='text-[30px] font-bold mb-[46px]'>
         Showing Results
@@ -14,12 +29,27 @@ const Exercises = ({setExercises, bodyPart, exercises}) => {
 
       <div className='flex flex-row flex-wrap justify-center'>
         {
-          exercises.map((exercise, index) => (
+          currentExercises.map((exercise, index) => (
             <ExerciseCard key={index} exercise={exercise}/>
           ))
         }
       </div>
 
+      <div className='flex justify-center'>
+        {exercises.length > 9 && (
+          <Pagination
+            color='standard'
+            shape='rounded'
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          > 
+          </Pagination>
+        )}
+          
+      </div> 
     </div>
   )
 }
